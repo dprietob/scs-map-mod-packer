28a6a2dacaed484c0424e220ba50983927ea3459
Daniel Prieto <priberda@gmail.com>
2020-08-27 09:35:19 +0200

    License comment class added. How to use url in about dialog fixed. CLI example help commands fixed.

--- src/com/scsmmp/Packer.java
+++ src/com/scsmmp/Packer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2020 The SCSMMP Author
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.scsmmp;
 
 import com.scsmmp.interfaces.ProcessUpdaterListener;
@@ -15,8 +31,6 @@ import java.util.zip.ZipOutputStream;
  * Class that defines the Packer entity, in charge to wrapping the mod.
  *
  * @author Daniel Prieto
- * @version 1.0.0
- * @since 2020-08-26
  */
 public class Packer
 {
@@ -59,8 +73,6 @@ public class Packer
      * wrapping process.
      *
      * @author Daniel Prieto
-     * @version 1.0.0
-     * @since 2020-08-26
      */
     private class PackerThread extends Thread implements Runnable
     {
